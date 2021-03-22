cd47bd941757f92a2272d7c7145613857c64506f
Wojciech Sznapka <wojciech@sznapka.pl>
2021-03-22 21:23:37 +0100

    Refactor

--- detect.py
+++ detect.py
@@ -1,60 +1,57 @@
 #!/usr/bin/env python3.7
 import logging
-import os
 
-logging.basicConfig(level=logging.INFO, format='[%(relativeCreated)d] %(asctime)s :: %(message)s')
+logging.basicConfig(level=logging.INFO, format='[%(relativeCreated)d] %(asctime)s :%(levelname)s: %(message)s')
 logging.info('Starting')
-
-if 'PUSHBULLET_API_KEY' not in os.environ:
-    raise 'PUSHBULLET_API_KEY not defined in env variables'
-
-PUSHBULLET_API_KEY = os.environ['PUSHBULLET_API_KEY']
 
 from PIL import Image
 from datetime import datetime, timedelta
 from utils import label_map_util
 from utils import visualization_utils as vis_util
-import argparse
 import cv2
-import glob
 import numpy as np
+import os
 import requests
-import sys
 import tensorflow as tf
 import time
+
 logging.info('Imports done')
 
 tf.compat.v1.enable_eager_execution()
+PUSHBULLET_API_KEY = os.environ.get('PUSHBULLET_API_KEY', None)
+CAMERA_IMAGE = 'https://pihome.sznapka.pl/camera/auto.jpg'
+
 
 def send_notification(title, body, link, api_key, latest_notification):
     if latest_notification and datetime.now() - timedelta(seconds=60) < latest_notification:
         return latest_notification
 
-    data = { 
+    data = {
         'title': title,
         'body': body,
         'type': 'link',
         'url': link
-    }   
+    }
     headers = {'Access-Token': api_key}
     requests.post('https://api.pushbullet.com/v2/pushes', data=data, headers=headers)
     logging.info('Sent notification with {0}'.format(link))
     return datetime.now()
 
+
 MODEL_NAME = 'ssdlite_mobilenet_v2_coco_2018_05_09'
 CWD_PATH = os.getcwd()
-PATH_TO_CKPT = os.path.join(CWD_PATH,MODEL_NAME,'frozen_inference_graph.pb')
-PATH_TO_LABELS = os.path.join(CWD_PATH,'models/research/object_detection/data','mscoco_label_map.pbtxt')
+PATH_TO_GRAPH = os.path.join(CWD_PATH, MODEL_NAME, 'frozen_inference_graph.pb')
+PATH_TO_LABELS = os.path.join(CWD_PATH, 'models/research/object_detection/data', 'mscoco_label_map.pbtxt')
 NUM_CLASSES = 90
 label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
-categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES,
+                                                            use_display_name=True)
 category_index = label_map_util.create_category_index(categories)
 
-# Load the Tensorflow model into memory.
 detection_graph = tf.Graph()
 with detection_graph.as_default():
     od_graph_def = tf.compat.v1.GraphDef()
-    with tf.io.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+    with tf.io.gfile.GFile(PATH_TO_GRAPH, 'rb') as fid:
         serialized_graph = fid.read()
         od_graph_def.ParseFromString(serialized_graph)
         tf.import_graph_def(od_graph_def, name='')
@@ -67,23 +64,22 @@ detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
 num_detections = detection_graph.get_tensor_by_name('num_detections:0')
 
 logging.info('All initialization done')
-latest_notification = None
+notification = None
 
-excluded_classes = ['train', 'umbrella', 'kite', 'boat', 'zebra']
+excluded_classes = ['train', 'umbrella', 'kite', 'boat', 'zebra', 'clock']
 rootpath = '/var/www/sznapka.pl/'
 notification_host = 'https://sznapka.pl/'
-camera_image = 'https://pihome.sznapka.pl/camera/auto.jpg'
 night_hours = range(5, 21)
 THRESHOLD = .5
 
 while True:
-#for path in glob.glob('/tmp/w*'):
+    # for path in glob.glob('/tmp/w*'):
     path = '/tmp/camera.jpg'
     logging.info('Fetching image to {}'.format(path))
     outpath = 'detection/camera-detected-{}.jpg'.format(datetime.now().strftime('%Y%m%d_%H%M'))
 
     try:
-        r = requests.get(camera_image)
+        r = requests.get(CAMERA_IMAGE)
         if r.status_code != 200:
             raise "Error {0}: {1}".format(r.status_code, r.content)
         with open(path, 'wb') as f:
@@ -106,8 +102,9 @@ while True:
                     continue
                 else:
                     current_classes.append((class_name, val))
-                    logging.warning('Found at index {} class: {} with {:.0f}% boxes: {}'.format(idx, class_name, val * 100, boxes[0][idx]))
-
+                    logging.warning(
+                        'Found at index {} class: {} with {:.0f}% boxes: {}'.format(idx, class_name, val * 100,
+                                                                                    boxes[0][idx]))
 
         if len(current_classes) > 0:
             cv2.imwrite(rootpath + outpath.replace('detected', 'original'), image)
@@ -123,8 +120,9 @@ while True:
             cv2.imwrite(rootpath + outpath, image)
             detection_msg = ', '.join(map(lambda x: "{}: {:.0f}%".format(x[0], x[1] * 100), current_classes))
             logging.info('Wrote to {0}'.format(outpath))
-            latest_notification = send_notification('Detected: {}'.format(detection_msg), datetime.now().strftime('%T'),
-                    notification_host + outpath, PUSHBULLET_API_KEY, latest_notification)
+            if PUSHBULLET_API_KEY:
+                notification = send_notification('Detected: {}'.format(detection_msg), datetime.now().strftime('%T'),
+                                                 notification_host + outpath, PUSHBULLET_API_KEY, notification)
 
         hour = int(datetime.now().strftime('%H'))
         if hour not in night_hours:
